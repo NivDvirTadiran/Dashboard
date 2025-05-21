35b567e393bead7693e1411c18c15f2d41ebf359
NivDvirTadiran <nivdvirtadirantele@gmail.com>
2025-05-21 18:19:18 +0300

    ACC Dashboard | Add loading indicator component and update widget styles for improved UI

---
+++ Frontend/src/stories/sb-gridstack/loading/loading-indicator.component.ts
@@ -0,0 +1,48 @@
+import { Component, ContentChild, Input, OnInit, TemplateRef } from "@angular/core";
+import { LoadingService } from "./loading.service";
+import { Observable } from "rxjs";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { AsyncPipe, NgIf, NgTemplateOutlet } from "@angular/common";
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from "@angular/router";
+import { tap } from "rxjs/operators";
+
+
+@Component({
+    selector: "loading-indicator",
+    templateUrl: "./loading-indicator.component.html",
+    styleUrls: ["./loading-indicator.component.scss"],
+    imports: [MatProgressSpinnerModule, AsyncPipe, NgIf, NgTemplateOutlet],
+    standalone: true,
+})
+export class LoadingIndicatorComponent implements OnInit {
+
+    loading$: Observable<boolean>;
+
+    @Input()
+    detectRouteTransitions = false;
+
+    @ContentChild("loading")
+    customLoadingIndicator: TemplateRef<any> | null = null;
+
+    constructor(
+        private loadingService: LoadingService,
+        private router: Router) {
+        this.loading$ = this.loadingService.loading$;
+    }
+
+    ngOnInit() {
+        if (this.detectRouteTransitions) {
+            this.router.events
+                .pipe(
+                    tap((event) => {
+                        if (event instanceof RouteConfigLoadStart) {
+                            this.loadingService.loadingOn();
+                        } else if (event instanceof RouteConfigLoadEnd) {
+                            this.loadingService.loadingOff();
+                        }
+                    })
+                )
+                .subscribe();
+        }
+    }
+}
